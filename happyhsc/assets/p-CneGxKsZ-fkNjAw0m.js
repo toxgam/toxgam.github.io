79a70f49450dd23755904cbe381bcb18017c4128
Ngoc Dao <ngocdaothanh@gmail.com>
2026-06-26 22:42:01 +1000

    Update happyhsc

--- happyhsc/assets/p-CneGxKsZ-fkNjAw0m.js
+++ happyhsc/assets/p-CneGxKsZ-fkNjAw0m.js
@@ -1,3 +1,3 @@
-import{W as r,f as s,e as i,P as a,m}from"./index-CXLItJvX.js";/*!
+import{W as r,f as s,e as i,P as a,m}from"./index-B_N5605K.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const c=()=>{const t=window;t.addEventListener("statusTap",()=>{r(()=>{const n=document.elementFromPoint(t.innerWidth/2,t.innerHeight/2);if(!n)return;const e=s(n);e&&new Promise(o=>i(e,o)).then(()=>{a(async()=>{e.style.setProperty("--overflow","hidden"),await m(e,300),e.style.removeProperty("--overflow")})})})})};export{c as startStatusTap};
